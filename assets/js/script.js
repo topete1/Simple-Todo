caa8834c8ab2a33b47ae3152b1cb46c3a15717aa
jose <josetopete1@gmail.com>
2017-12-18 15:59:59 +0800

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function bing() {
+    console.log("bing!");
+}
+
+function bong() {
+    console.log("bong!");
+}
+
+//Checks off todo on click
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+
+//Click on X to delete todo
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation(); // jQuery function to stop bubbling of code.
+});
+
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        // grab new todo text from input
+        var todoText = $(this).val();
+        //clear input
+        $(this).val("");
+        // create a new li and add into ul (append)
+        $("ul").append('<li><span><i class="fa fa-times" aria-hidden="true"></i></span>' + todoText + '</li>');
+    }
+});
+
+$(".fa-plus").on("click",function(){
+    $("input[type='text']").fadeToggle();
+});
